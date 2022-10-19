644663987559c172f4182572883efa6fbaec2c1e
rmadbstjd <rmadbstjd@naver.com>
2022-10-19 10:30:34 +0900

    JSX안에서 if문을 사용하지 않고 삼항연산자를 통해 해결

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -3,17 +3,17 @@ import Profile from './components/Profile';
 
 function AppProfile() {
   const profileArr = [{img : "https://plus.unsplash.com/premium_photo-1663050629719-83e16832a804?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
-                      name : "black kim", job : "BE Developer"},
+                      name : "black kim", job : "BE Developer", state : "new"},
                       {img :"https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1085&q=80", 
-                      name : "holly molly", job : "FE Developer"},
+                      name : "holly molly", job : "FE Developer", state : "old"},
                       {img : "https://images.unsplash.com/photo-1657000521605-559d5e227280?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-                      name : "Bob kyu", job :"Designer"}];
+                      name : "Bob kyu", job :"Designer", state : "old"}];
   return(
     <> 
       {
           profileArr.map((item) => {
             return <Profile img = {item.img}
-            name= {item.name} title={item.job}/>
+            name= {item.name} title={item.job} state={item.state}/>
           })
       }
     </>
