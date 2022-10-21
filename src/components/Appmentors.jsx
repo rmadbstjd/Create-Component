0286f3aff8af5f63a7227d8395be6c1e74b973f0
rmadbstjd <rmadbstjd@naver.com>
2022-10-21 22:33:33 +0900

    배열형태의  상태 관리를 위한 연습

--- src/components/Appmentors.jsx
+++ src/components/Appmentors.jsx
@@ -31,17 +31,36 @@ export default function AppMentor() {
         onClick={() => {
           const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
           const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-          setPerson({...person, mentors : person.mentors.map((mentor)=> {
+          setPerson({...person, mentors : person.mentors.map((mentor) => {
             if(mentor.name === prev) {
                 return {...mentor, name : current};
             }
-            return mentor;
-          })  
-        });
+            else {
+                return mentor;
+            }
+          })});
         }}
       >
         멘토의 이름을 바꾸기
       </button>
+      <button onClick={() => {
+        const addName = prompt(`추가할 멘토의 이름을 입력하세요`);
+        const addTitle = prompt(`추가할 직업을 입력하세요`);
+        /*let arr = person.mentors;
+        arr.push({name : addName, title : addTitle});
+        setPerson({...person, mentors : arr});*/
+        setPerson({...person, mentors : [...person.mentors, {name : addName, title : addTitle}]});
+        
+      }}>멘토 추가하기</button>
+      <button onClick={() => {
+        const deleteName = prompt(`삭제할 멘토의 이름을 입력하세요`); 
+        
+        setPerson({...person, mentors : person.mentors.filter((mentor) => {
+            if(deleteName !== mentor.name) {
+                return mentor; 
+            }
+        })});
+      }}>멘토 삭제하기</button>
     </div>
   );
 }
